90b591245105425a1a7537576dfc312ec2da274c
careypjr <careypjr@yahoo.com>
2015-08-02 19:35:31 -0500

    Updated Page

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ include 'icl/header.php';
 </div>
 <div id="feed">
 	<div id="News-Title">
-		<h1>Server Updates</h1>		
+		<h1>Server Updates</h1>
 	</div>
-	
+
 <?php
 mysql_connect('127.0.0.1','feed','katie11');
 mysql_select_db('news');
@@ -24,7 +24,7 @@ while($output = mysql_fetch_assoc($query))
 	echo date('D-M-Y', $output['date']).'<br / >';
 	echo 'Posted by '.$output['postedby'].'<br / >';
 	echo $output['news'].'<br / >';
-	echo '<hr />'; 
+	echo '<hr />';
 }
 ?>
 </div>
@@ -32,9 +32,13 @@ while($output = mysql_fetch_assoc($query))
 	<h3>Wanna Play Shaiya Ep 4.5?</h3>
 	<p>Come Join Us On The 4.5 Server. Staff Positions Are Open. Players Are Friendly, Developers Are Active, And Best Of All Its Free To Play!
 		Please Note That Donation Points Are Not Currently Dropping</p>
+    <button> Play 4.5!</button>
 </div>
-
-
+<div id="five">
+  <h3>Wanna Play Shaiya Ep 5.4?</h3>
+  <p>Currently The 5.4 Server Is Uner Construction Please Bare With Us While We Build It</p>
+<button>Play 5.4!</button>
+</div>
 
 
 
